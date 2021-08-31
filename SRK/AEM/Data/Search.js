db325e3f063cc573b21dfc1bba8a8f4a67843dd9
agathalai <agatha@agathalai.com>
2021-08-31 12:01:07 -0700

    added missing memo files

--- SRK/AEM/Data/Search.js
+++ SRK/AEM/Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/GeoChem/GeoChem_AcquisitionReqs.htm'],s:['$10','mineralog'],p:[['$10',23],['block',27],['examples',18],['later',3],['plot',14],['studies',20]]});
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/GeoChem/GeoChem_AcquisitionReqs.htm'],s:['$10','mineralog'],p:[['$10',23],['block',27],['examples',5],['late',29],['plot',5],['studies',9]]});
--- SRK/AEM/Resources/Scripts/require.config.js
+++ SRK/AEM/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=637659475394596146'
+    urlArgs: 't=637660079757891451'
 });
