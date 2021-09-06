70e1526e0746db3e86adc310f12ffedfd983e104
agathalai <agatha@agathalai.com>
2021-09-06 16:20:24 -0700

    adding SeabeeOMS mockup

---
+++ SRK/SeabeeOMS/Data/SearchUrl_Chunk0.js
@@ -0,0 +1,1 @@
+define({"../Content/SeabeeOMS/OMS_guidelines.htm":2,"../Content/SeabeeOMS/OMS_intro.htm":3,"../Content/SeabeeOMS/OMS_ref.htm":4,"../Content/SeabeeOMS/OMS_revHistory.htm":1,"../Content/glossary.htm":0,});
---
+++ SRK/SeabeeOMS/Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=637665417125810629'
+});
