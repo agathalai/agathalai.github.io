c172efd9339e5a3154cc16db7d662af616cd25a6
agathalai <agatha@agathalai.com>
2021-09-18 11:22:51 -0700

    Added Tailings Facility chapter

--- SRK/SeabeeOMS/Data/Search.js
+++ SRK/SeabeeOMS/Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/SeabeeOMS/OMS_DocControl.htm'],s:['0'],p:[['0',1],['living',6]]});
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/SeabeeOMS/OMS_DocControl.htm'],s:['0','recurr'],p:[['0',1],['claude',17],['hydraulic',5],['post',13],['tmf',14]]});
--- SRK/SeabeeOMS/Resources/Scripts/require.config.js
+++ SRK/SeabeeOMS/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=637670501527450565'
+    urlArgs: 't=637675608051617409'
 });
