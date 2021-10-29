e9641b9b6a36c60d3b1bc8ff39df3c18a75db60f
agathalai <agatha@agathalai.com>
2021-10-29 15:40:40 -0700

    added Appendix D content

--- SRK/SeabeeOMS/Data/Search.js
+++ SRK/SeabeeOMS/Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/SeabeeOMS/OMS_DocControl.htm'],s:['0','for','separ'],p:[['0',1],['an',47],['changes',17],['dike',14],['from',18],['layers',15],['of',47],['record',32],['srk',53],['tmf',24]]});
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/SeabeeOMS/OMS_DocControl.htm'],s:['0','float','schedul'],p:[['0',1],['along',15],['cannot',50],['design',57],['forms',25],['lake',14],['of',9],['quality',19],['sliding',46],['they',3]]});
--- SRK/SeabeeOMS/Resources/Scripts/require.config.js
+++ SRK/SeabeeOMS/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=637710381833243256'
+    urlArgs: 't=637711187601378267'
 });
